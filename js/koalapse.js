46b8c472050b82db220902333fa235d3bdd2a407
chrishrmnn <contact@christophe-hermann.fr>
2014-12-09 15:05:25 +0100

    Minimal CSS Reduce the CSS required

--- js/koalapse.js
+++ js/koalapse.js
@@ -15,7 +15,7 @@
             "closeOthers": true,
             "animated": false
             },
-            parametres = $.extend(defauts, options),
+            parameters = $.extend(defauts, options),
             $_that = this;
 
 
@@ -25,7 +25,7 @@
             var $_this = $(this),
                 id = "koalapse-" + $_this.index(),
                 kHeadingHTML = $_this.html(),
-                kContent = $_this.next('.'+ parametres.contentClass);
+                kContent = $_this.next('.'+ parameters.contentClass);
 
             // Wrap heading content in a button and add WAI-ARIA attributes
             $_this.html('<button class="koalapse__trigger" aria-expanded="false" aria-controls="'+ id +'">'+ kHeadingHTML +'</button>');
@@ -43,9 +43,9 @@
                 var state = $(this).attr('aria-expanded') === 'false' ? true : false;
 
                 // Close others - if options is set to true
-                if( parametres.closeOthers === true ){
+                if( parameters.closeOthers === true ){
                     $('.koalapse__trigger').attr('aria-expanded', false);
-                    $('.'+ parametres.contentClass).attr('aria-hidden', true);
+                    $('.'+ parameters.contentClass).attr('aria-hidden', true);
                 }
 
                 // Show the selected content
