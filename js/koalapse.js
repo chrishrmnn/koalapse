2acc37f357fd3dddf011b58d5336bde03d5b3295
chrishrmnn <contact@christophe-hermann.fr>
2014-12-09 17:58:44 +0100

    Keys navigation Navigate between heading with u/left/down/right/end/home keys. Open close panel using enter/space

--- js/koalapse.js
+++ js/koalapse.js
@@ -16,7 +16,8 @@
             "animated": false
             },
             parameters = $.extend(defauts, options),
-            $_that = this;
+            $_that = this,
+            focusOnHeading;
 
 
         //
@@ -24,12 +25,15 @@
             // variables attached to the element
             var $_this = $(this),
                 id = "koalapse-" + $_this.index(),
-                kHeadingHTML = $_this.html(),
                 kPanel = $_this.next('.'+ parameters.panelClass);
 
-            // Wrap heading content in a button and add WAI-ARIA attributes
-            $_this.html('<button class="koalapse__trigger" aria-expanded="false" aria-controls="'+ id +'">'+ kHeadingHTML +'</button>');
-            var trigger = $_this.children('.koalapse__trigger');
+
+            // Add WAI-ARIA attributes and make it focusable
+            $_this.attr({
+                'aria-expanded': false,
+                'aria-controls': id,
+                'tabindex': 0
+            });
 
             // Add WAI-ARIA attributes to the content related to the title
             kPanel.attr({
@@ -38,7 +42,7 @@
             });
 
             // Events
-            trigger.on('click', function(){
+            $_this.on('click', function(){
                 // Get current state
                 var state = $(this).attr('aria-expanded') === 'false' ? true : false;
 
@@ -51,8 +55,58 @@
                 // Show the selected content
                 $(this).attr('aria-expanded', state);
                 kPanel.attr('aria-hidden', !state);
-            });
+            }).on('keydown', function(e){
+                var activeEl = $(document.activeElement);
 
+                console.log(e.keyCode);
+
+
+                // If the active element - with focus - is a title
+                if( activeEl.is($_this) ){
+                    focusOnHeading = true;
+                }
+
+                if( focusOnHeading ){
+                    // Enter toggle the panel
+                    if( e.keyCode === 13  || e.keyCode === 32 ){
+                        $(this).click();
+                    }
+
+                    // Left and Up arrows : focus the next heading
+                    if( e.keyCode === 37 || e.keyCode === 38 ){
+                        // If it's first heading - focus on last
+                        if( activeEl.is('.koalapse__title:first') ){
+                            activeEl.nextAll('.koalapse__title').last().focus();
+                        } else {
+                            activeEl.prevAll('.koalapse__title').first().focus();
+                        }
+                    }
+
+                    // Right and Down arrows : focus the next heading
+                    if( e.keyCode === 39 || e.keyCode === 40 ){
+                        // If it's last heading - focus on first
+                        if( activeEl.is('.koalapse__title:last') ){
+                            console.log('last');
+                            activeEl.prevAll('.koalapse__title').last().focus();
+                        } else {
+                            activeEl.nextAll('.koalapse__title').first().focus();
+                        }
+                    }
+
+                    // Home : focus the first heading
+                    if( e.keyCode === 36 ){
+                        $('.koalapse__title:first').focus();
+                    }
+
+                    // End : focus the last heading
+                    if( e.keyCode === 35 ){
+                        $('.koalapse__title:last').focus();
+                    }
+
+                } else {
+
+                }
+            });
         });
     };
 })(jQuery);
