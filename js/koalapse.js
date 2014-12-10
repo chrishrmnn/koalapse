99b3a4daf487f39f3fcec6bb052ec13e76112a48
chrishrmnn <contact@christophe-hermann.fr>
2014-12-10 16:38:34 +0100

    Accordion fully functionnal - Multiple accordion on one page - Keyboard navigation ( based on http://www.oaa-accessibility.org/example/35/ )

--- js/koalapse.js
+++ js/koalapse.js
@@ -10,103 +10,142 @@
     $.fn.koalapse=function(options){
 
         // variables used in the plugin
-        var defauts = {
-            "panelClass": "koalapse__content",
-            "closeOthers": true,
-            "animated": false
+        var defaults = {
+                "parentClass": ".koalapse",
+                "panelClass": ".koalapse__content",
+                "headingClass": ".koalapse__title",
+                "closeOthers": false,
+                "animated": false,
+                "showContentOnFocus": false
             },
-            parameters = $.extend(defauts, options),
-            $_that = this,
-            focusOnHeading;
+            parameters = $.extend(defaults, options),
+            $_that = this;
 
 
         //
-        return $_that.each(function(){
+        $_that.each(function(){
             // variables attached to the element
-            var $_this = $(this),
-                id = "koalapse-" + $_this.index(),
-                kPanel = $_this.next('.'+ parameters.panelClass);
+            var kParent = $(this),
+                kHeading = kParent.find(parameters.headingClass);
 
 
-            // Add WAI-ARIA attributes and make it focusable
-            $_this.attr({
-                'aria-expanded': false,
-                'aria-controls': id,
-                'tabindex': 0
-            });
+            // Link title and panel together with ID and ARIA attributes.
+            kHeading.each(function(){
+                var id = "koalapse-" + kParent.index() + $(this).index(),
+                    kPanel = $(this).next(parameters.panelClass);
 
-            // Add WAI-ARIA attributes to the content related to the title
-            kPanel.attr({
-                'id': id,
-                'aria-hidden': true
-            });
+                // Add WAI-ARIA attributes and make it focusable
+                $(this).attr({
+                    'aria-expanded': false,
+                    'aria-controls': id,
+                    'tabindex': 0
+                });
 
-            // Events
-            $_this.on('click', function(){
-                // Get current state
-                var state = $(this).attr('aria-expanded') === 'false' ? true : false;
-
-                // Close others - if options is set to true
-                if( parameters.closeOthers === true ){
-                    $('.koalapse__trigger').attr('aria-expanded', false);
-                    $('.'+ parameters.panelClass).attr('aria-hidden', true);
-                }
-
-                // Show the selected content
-                $(this).attr('aria-expanded', state);
-                kPanel.attr('aria-hidden', !state);
-            }).on('keydown', function(e){
-                var activeEl = $(document.activeElement);
-
-                console.log(e.keyCode);
-
-
-                // If the active element - with focus - is a title
-                if( activeEl.is($_this) ){
-                    focusOnHeading = true;
-                }
-
-                if( focusOnHeading ){
-                    // Enter toggle the panel
-                    if( e.keyCode === 13  || e.keyCode === 32 ){
-                        $(this).click();
-                    }
-
-                    // Left and Up arrows : focus the next heading
-                    if( e.keyCode === 37 || e.keyCode === 38 ){
-                        // If it's first heading - focus on last
-                        if( activeEl.is('.koalapse__title:first') ){
-                            activeEl.nextAll('.koalapse__title').last().focus();
-                        } else {
-                            activeEl.prevAll('.koalapse__title').first().focus();
-                        }
-                    }
-
-                    // Right and Down arrows : focus the next heading
-                    if( e.keyCode === 39 || e.keyCode === 40 ){
-                        // If it's last heading - focus on first
-                        if( activeEl.is('.koalapse__title:last') ){
-                            console.log('last');
-                            activeEl.prevAll('.koalapse__title').last().focus();
-                        } else {
-                            activeEl.nextAll('.koalapse__title').first().focus();
-                        }
-                    }
-
-                    // Home : focus the first heading
-                    if( e.keyCode === 36 ){
-                        $('.koalapse__title:first').focus();
-                    }
-
-                    // End : focus the last heading
-                    if( e.keyCode === 35 ){
-                        $('.koalapse__title:last').focus();
-                    }
-
-                } else {
-
-                }
+                // Add WAI-ARIA attributes to the content related to the title
+                kPanel.attr({
+                    'id': id,
+                    'aria-hidden': true
+                });
             });
         });
+
+
+
+        // Events
+        $('body').on('click', parameters.headingClass, function(){
+            var $_this = $(this),
+                state = $_this.attr('aria-expanded') === 'false' ? true : false,
+                controledEl = $('#' + $_this.attr('aria-controls')),
+                parent = $_this.parents(parameters.parentClass);
+
+            // Close others - if options is set to true
+            if( parameters.closeOthers === true ){
+                $(parameters.headingClass, parent).attr('aria-expanded', false);
+                $(parameters.panelClass, parent).attr('aria-hidden', true);
+            }
+
+            // Show the selected content
+            $_this.attr('aria-expanded', state);
+            controledEl.attr('aria-hidden', !state);
+
+        }).on('keydown', parameters.headingClass, function(e){
+            var $_this = $(this),
+                activeEl = $(document.activeElement),
+                parent = activeEl.parents(parameters.parentClass);
+
+                console.log(activeEl);
+
+                // Enter and Space toggle the panel
+                if( e.keyCode === 13  || e.keyCode === 32 ){
+                    $(this).click();
+                }
+
+                // Left and Up arrows : focus the next heading
+                if( e.keyCode === 37 || e.keyCode === 38 ){
+                    // If it's first heading - focus on last
+                    if( activeEl[0] == parent.find(parameters.headingClass).first()[0] ){
+                        activeEl.nextAll(parameters.headingClass).last().focus();
+                    } else {
+                        activeEl.prevAll(parameters.headingClass).first().focus();
+                    }
+                }
+
+                // Right and Down arrows : focus the next heading
+                if( e.keyCode === 39 || e.keyCode === 40 ){
+                    // If it's last heading - focus on first
+                    if( activeEl[0] == parent.find(parameters.headingClass).last()[0] ){
+                        activeEl.prevAll(parameters.headingClass).last().focus();
+                    } else {
+                        activeEl.nextAll(parameters.headingClass).first().focus();
+                    }
+                }
+
+                // Home : focus the first heading
+                if( e.keyCode === 36 ){
+                    parent.find(parameters.headingClass).first()[0].focus();
+                }
+
+                // End : focus the last heading
+                if( e.keyCode === 35 ){
+                    parent.find(parameters.headingClass).last()[0].focus();
+                }
+
+        }).on('keydown', parameters.panelClass, function(e){
+            var $_this = $(this),
+                activeEl = $(document.activeElement),
+                panel = activeEl.parents(parameters.panelClass),
+                panelID = panel.attr('id'),
+                parent = activeEl.parents(parameters.parentClass),
+                heading = $(parameters.headingClass + '[aria-controls='+ panelID +']');
+
+            // CTRL + Left or Up arrows : focus on heading of the "active" panel
+            if( (e.keyCode === 37 || e.keyCode === 38) && e.ctrlKey ){
+                heading.focus();
+            }
+
+            // CTRL + Page Up : show previous tab and focus previous heading
+            if( e.keyCode === 33 && e.ctrlKey ){
+                // If it's first heading - focus on last
+                if( heading[0] == parent.find(parameters.headingClass).first()[0] ){
+                    heading.nextAll(parameters.headingClass).last().focus().click();
+                } else {
+                    heading.prevAll(parameters.headingClass).first().focus().click();
+                }
+            }
+
+            // CTRL + Page Down : show next tab and focus previous heading
+            if( e.keyCode === 34 && e.ctrlKey ){
+                // If it's first heading - focus on last
+                if( heading[0] == parent.find(parameters.headingClass).last()[0] ){
+                    heading.prevAll(parameters.headingClass).last().focus().click();
+                } else {
+                    heading.nextAll(parameters.headingClass).first().focus().click();
+                }
+            }
+        });
+
+
+        return $_that;
+
     };
 })(jQuery);
